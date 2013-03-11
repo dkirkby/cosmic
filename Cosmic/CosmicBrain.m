17e2dcff5c9b40ba3c7e5a77f965b236007fb43e
David Kirkby <dkirkby@uci.edu>
2013-03-11 21:57:02 +0100

    Flesh out camera initialization a bit

--- Cosmic/CosmicBrain.m
+++ Cosmic/CosmicBrain.m
@@ -17,7 +17,28 @@
 
 - (void) initCapture {
     NSLog(@"initializing capture...");
+    // Initialize a session with the photo preset
     self.captureSession = [[AVCaptureSession alloc] init];
+    if([self.captureSession canSetSessionPreset:AVCaptureSessionPresetPhoto]) {
+        self.captureSession.sessionPreset = AVCaptureSessionPresetPhoto;
+    }
+    else {
+        NSLog(@"Failed to set the photo session preset!");
+    }
+    // Look for an input device
+    NSArray *cameras=[AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    for(AVCaptureDevice *device in cameras) {
+        NSLog(@"found %@",device.localizedName);
+    }
+    // Set the input device
+    NSError *error = nil;
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:[cameras objectAtIndex:0] error:&error];
+    if(!input) {
+        NSLog(@"PANIC: no media input");
+    }
+    else {
+        [self.captureSession addInput:input];
+    }
 }
 
 - (void) captureImage {
