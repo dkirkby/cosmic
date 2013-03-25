fee3035fbd129fddf1839bad9f9e03dd651594db
David Kirkby <dkirkby@uci.edu>
2013-03-25 20:59:21 +0100

    Add an exposureCount label

--- Cosmic/CosmicViewController.h
+++ Cosmic/CosmicViewController.h
@@ -12,5 +12,6 @@
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIButton *goButton;
+@property (weak, nonatomic) IBOutlet UILabel *exposureCount;
 
 @end
