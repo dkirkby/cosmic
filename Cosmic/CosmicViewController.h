997af388d23a8c943dc2fa3040ad5a6dfd9be791
Dylan Kirkby <dylan.kirkby@gmail.com>
2013-05-14 18:47:23 +0200

    trivial change

--- Cosmic/CosmicViewController.h
+++ Cosmic/CosmicViewController.h
@@ -2,7 +2,7 @@
 //  CosmicViewController.h
 //  Cosmic
 //
-//  Created by David Kirkby on 3/11/13.
+//  Created by David Kirkby and Dylan Kirkby on 3/11/13.
 //  Copyright (c) 2013 David Kirkby. All rights reserved.
 //
 
