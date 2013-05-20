e6569deb9256d3df395062f87b68ac15589a8e16
Dylan Kirkby <dylan.kirkby@gmail.com>
2013-05-20 13:08:01 +0200

    Added CosmicStampView to display data of stamp

---
+++ Cosmic/CosmicStampView.h
@@ -0,0 +1,13 @@
+//
+//  CosmicStampView.h
+//  Cosmic
+//
+//  Created by Dylan Kirkby on 5/20/13.
+//  Copyright (c) 2013 David Kirkby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CosmicStampView : UIView
+
+@end
---
+++ Cosmic/CosmicStampView.m
@@ -0,0 +1,31 @@
+//
+//  CosmicStampView.m
+//  Cosmic
+//
+//  Created by Dylan Kirkby on 5/20/13.
+//  Copyright (c) 2013 David Kirkby. All rights reserved.
+//
+
+#import "CosmicStampView.h"
+
+@implementation CosmicStampView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
